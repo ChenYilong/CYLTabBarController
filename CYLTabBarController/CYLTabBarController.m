9880767c21d08263e7ab4991b28345d83aaf5540
chenyilong <luohanchenyilong@163.com>
2015-11-04 01:28:38 +0800

    v1.0.5 remove useless code

--- CYLTabBarController/CYLTabBarController.m
+++ CYLTabBarController/CYLTabBarController.m
@@ -102,21 +102,6 @@ NSUInteger CYLTabbarItemsCount = 0;
     
 }
 
-- (UIImage *)imageWithColor:(UIColor *)color
-{
-    NSParameterAssert(color != nil);
-    
-    CGRect rect = CGRectMake(0, 0, 1, 1);
-    // Create a 1 by 1 pixel context
-    UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
-    [color setFill];
-    UIRectFill(rect);   // Fill it with your color
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    return image;
-}
-
 @end
 
 #pragma mark - UIViewController+CYLTabBarControllerItem
