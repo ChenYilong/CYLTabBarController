c4f9f24f57e688546a1d3f6f572dce479f1aceb5
chenyilong <luohanchenyilong@163.com>
2016-04-01 00:10:18 +0800

    v1.2.2 fix bug : if reload tabbar background turn out to be wrong

--- CYLTabBarController/CYLTabBar.m
+++ CYLTabBarController/CYLTabBar.m
@@ -138,17 +138,4 @@
     return nil;
 }
 
-- (UIImage *)imageWithColor:(UIColor *)color {
-    NSParameterAssert(color != nil);
-    
-    CGRect rect = CGRectMake(0, 0, 1, 1);
-    // Create a 1 by 1 pixel context
-    UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
-    [color setFill];
-    UIRectFill(rect);   // Fill it with your color
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return image;
-}
-
 @end
