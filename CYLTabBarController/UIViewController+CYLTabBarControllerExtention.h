aa14d6ce71ba06ca7235da7baa87a715226f0c02
chenyilong <luohanchenyilong@163.com>
2016-12-26 11:32:46 +0800

    v1.8.0 add new API `-pushOrPopToViewController:animated:callback:`

--- CYLTabBarController/UIViewController+CYLTabBarControllerExtention.h
+++ CYLTabBarController/UIViewController+CYLTabBarControllerExtention.h
@@ -2,13 +2,21 @@
 //  UIViewController+CYLTabBarControllerExtention.h
 //  CYLTabBarController
 //
-//  v1.7.0 Created by 微博@iOS程序犭袁 ( http://weibo.com/luohanchenyilong/ ) on 16/2/26.
+//  v1.8.0 Created by 微博@iOS程序犭袁 ( http://weibo.com/luohanchenyilong/ ) on 16/2/26.
 //  Copyright © 2016年 https://github.com/ChenYilong .All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 typedef void (^CYLPopSelectTabBarChildViewControllerCompletion)(__kindof UIViewController *selectedTabBarChildViewController);
+
+typedef void (^CYLPushOrPopCompletionHandler)(BOOL shouldPop,
+                                              __kindof UIViewController *viewControllerPopTo,
+                                              BOOL shouldPopSelectTabBarChildViewController,
+                                              NSUInteger index
+                                              );
+
+typedef void (^CYLPushOrPopCallback)(NSArray<__kindof UIViewController *> *viewControllers, CYLPushOrPopCompletionHandler completionHandler);
 
 @interface UIViewController (CYLTabBarControllerExtention)
 
@@ -43,5 +51,26 @@ typedef void (^CYLPopSelectTabBarChildViewControllerCompletion)(__kindof UIViewC
  */
 - (void)cyl_popSelectTabBarChildViewControllerForClassType:(Class)classType
                                                 completion:(CYLPopSelectTabBarChildViewControllerCompletion)completion;
+
+/*!
+ *@brief 如果当前的 `NavigationViewController` 栈中包含有准备 Push 到的目标控制器，可以选择 Pop 而非 Push。
+ *@param viewController Pop 或 Push 到的“目标控制器”，由 completionHandler 的参数控制 Pop 和 Push 的细节。
+ *@param animated Pop 或 Push 时是否带动画
+ *@param callback 回调，如果传 nil，将进行 Push。callback 包含以下几个参数：
+                  *@param viewControllers 表示与“目标控制器”相同类型的控制器；
+                  *@param completionHandler 包含以下几个参数：
+                                            *@param shouldPop 是否 Pop
+                                            *@param viewControllerPopTo Pop 回的控制器
+                                            *@param shouldPopSelectTabBarChildViewController 在进行 Push 行为之前，是否 Pop 到当前 `NavigationController` 的栈底。
+                                                                                             可能的值如下：
+                                                                                             NO 如果上一个参数为 NO，下一个参数 index 将被忽略。
+                                                                                             YES 会根据 index 参数改变 `TabBarController` 的 `selectedViewController` 属性。
+                                                                                             注意：该属性在 Pop 行为时不起作用。
+                                             *@param index Pop 改变 `TabBarController` 的 `selectedViewController` 属性。
+                                                           注意：该属性在 Pop 行为时不起作用。
+                   */
+- (void)cyl_pushOrPopToViewController:(UIViewController *)viewController
+                             animated:(BOOL)animated
+                             callback:(CYLPushOrPopCallback)callback;
 
 @end
--- CYLTabBarController/UIViewController+CYLTabBarControllerExtention.m
+++ CYLTabBarController/UIViewController+CYLTabBarControllerExtention.m
@@ -2,7 +2,7 @@
 //  UIViewController+CYLTabBarControllerExtention.m
 //  CYLTabBarController
 //
-//  v1.7.0 Created by 微博@iOS程序犭袁 ( http://weibo.com/luohanchenyilong/ ) on 16/2/26.
+//  v1.8.0 Created by 微博@iOS程序犭袁 ( http://weibo.com/luohanchenyilong/ ) on 16/2/26.
 //  Copyright © 2016年 https://github.com/ChenYilong .All rights reserved.
 //
 
@@ -11,17 +11,16 @@
 
 @implementation UIViewController (CYLTabBarControllerExtention)
 
+#pragma mark -
+#pragma mark - public Methods
+
 - (UIViewController *)cyl_popSelectTabBarChildViewControllerAtIndex:(NSUInteger)index {
     [self checkTabBarChildControllerValidityAtIndex:index];
     [self.navigationController popToRootViewControllerAnimated:NO];
     CYLTabBarController *tabBarController = [self cyl_tabBarController];
     tabBarController.selectedIndex = index;
     UIViewController *selectedTabBarChildViewController = tabBarController.selectedViewController;
-    BOOL isNavigationController = [[selectedTabBarChildViewController class] isSubclassOfClass:[UINavigationController class]];
-    if (isNavigationController) {
-        return ((UINavigationController *)selectedTabBarChildViewController).viewControllers[0];
-    }
-    return selectedTabBarChildViewController;
+    return [selectedTabBarChildViewController cyl_getViewControllerInsteadIOfNavigationController];
 }
 
 - (void)cyl_popSelectTabBarChildViewControllerAtIndex:(NSUInteger)index
@@ -34,22 +33,8 @@
 
 - (UIViewController *)cyl_popSelectTabBarChildViewControllerForClassType:(Class)classType {
     CYLTabBarController *tabBarController = [self cyl_tabBarController];
-    __block NSInteger atIndex = NSNotFound;
-    [tabBarController.viewControllers enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        id obj_ = nil;
-        BOOL isNavigationController = [[tabBarController.viewControllers[idx] class] isSubclassOfClass:[UINavigationController class]];
-        if (isNavigationController) {
-            obj_ = ((UINavigationController *)obj).viewControllers[0];
-        } else {
-            obj_ = obj;
-        }
-        if ([obj_ isKindOfClass:classType]) {
-            atIndex = idx;
-            *stop = YES;
-            return;
-        }
-    }];
-    
+    NSArray *viewControllers = tabBarController.viewControllers;
+    NSInteger atIndex = [self cyl_indexForClassType:classType inViewControllers:viewControllers];
     return [self cyl_popSelectTabBarChildViewControllerAtIndex:atIndex];
 }
 
@@ -59,6 +44,75 @@
     dispatch_async(dispatch_get_main_queue(), ^{
         !completion ?: completion(selectedTabBarChildViewController);
     });
+}
+
+- (void)cyl_pushOrPopToViewController:(UIViewController *)viewController
+                             animated:(BOOL)animated
+                             callback:(CYLPushOrPopCallback)callback {
+    if (!callback) {
+        [self.navigationController pushViewController:viewController animated:animated];
+        return;
+    }
+    
+    void (^popSelectTabBarChildViewControllerCallback)(BOOL shouldPopSelectTabBarChildViewController, NSUInteger index) = ^(BOOL shouldPopSelectTabBarChildViewController, NSUInteger index) {
+        if (shouldPopSelectTabBarChildViewController) {
+            [self cyl_popSelectTabBarChildViewControllerAtIndex:index completion:^(__kindof UIViewController *selectedTabBarChildViewController) {
+                [selectedTabBarChildViewController.navigationController pushViewController:viewController animated:animated];
+            }];
+        } else {
+            [self.navigationController pushViewController:viewController animated:animated];
+        }
+    };
+    NSArray<__kindof UIViewController *> *otherSameClassTypeViewControllersInCurrentNavigationControllerStack = [self cyl_getOtherSameClassTypeViewControllersInCurrentNavigationControllerStack:viewController];
+    
+    CYLPushOrPopCompletionHandler completionHandler = ^(BOOL shouldPop,
+                                                        __kindof UIViewController *viewControllerPopTo,
+                                                        BOOL shouldPopSelectTabBarChildViewController,
+                                                        NSUInteger index
+                                                        ) {
+        if (!otherSameClassTypeViewControllersInCurrentNavigationControllerStack || otherSameClassTypeViewControllersInCurrentNavigationControllerStack.count == 0) {
+            shouldPop = NO;
+        }
+        dispatch_async(dispatch_get_main_queue(),^{
+            if (shouldPop) {
+                [self.navigationController popToViewController:viewControllerPopTo animated:animated];
+                return;
+            }
+            popSelectTabBarChildViewControllerCallback(shouldPopSelectTabBarChildViewController, index);
+        });
+    };
+    callback(otherSameClassTypeViewControllersInCurrentNavigationControllerStack, completionHandler);
+}
+
+#pragma mark -
+#pragma mark - Private Methods
+
+- (UIViewController *)cyl_getViewControllerInsteadIOfNavigationController {
+    BOOL isNavigationController = [[self class] isSubclassOfClass:[UINavigationController class]];
+    if (isNavigationController) {
+        return ((UINavigationController *)self).viewControllers[0];
+    }
+    return self;
+}
+
+- (NSArray<__kindof UIViewController *> *)cyl_getOtherSameClassTypeViewControllersInCurrentNavigationControllerStack:(UIViewController *)viewController {
+    NSArray *currentNavigationControllerStack = [self.navigationController viewControllers];
+    if (currentNavigationControllerStack.count < 2) {
+        return nil;
+    }
+    NSMutableArray *mutableArray = [currentNavigationControllerStack mutableCopy];
+    [mutableArray removeObject:self];
+    currentNavigationControllerStack = [mutableArray copy];
+    
+    __block NSMutableArray *mutableOtherViewControllersInNavigationControllerStack = [NSMutableArray arrayWithCapacity:currentNavigationControllerStack.count];
+    
+    [currentNavigationControllerStack enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        UIViewController *otherViewController = obj;
+        if ([otherViewController isKindOfClass:[viewController class]]) {
+            [mutableOtherViewControllersInNavigationControllerStack addObject:otherViewController];
+        }
+    }];
+    return [mutableOtherViewControllersInNavigationControllerStack copy];
 }
 
 - (void)checkTabBarChildControllerValidityAtIndex:(NSUInteger)index {
@@ -85,6 +139,19 @@
                                        reason:reason
                                      userInfo:nil];
     }
+}
+
+- (NSInteger)cyl_indexForClassType:(Class)classType inViewControllers:(NSArray *)viewControllers {
+    __block NSInteger atIndex = NSNotFound;
+    [viewControllers enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        UIViewController *obj_ = [obj cyl_getViewControllerInsteadIOfNavigationController];
+        if ([obj_ isKindOfClass:classType]) {
+            atIndex = idx;
+            *stop = YES;
+            return;
+        }
+    }];
+    return atIndex;
 }
 
 @end
