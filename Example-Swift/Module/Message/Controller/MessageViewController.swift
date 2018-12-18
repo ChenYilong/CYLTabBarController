2ce196cdae8f9371f6765857ac8f1e0ab8b06d5f
Elon Chan <luohanchenyilong@163.com>
2018-12-18 17:03:06 +0800

    udpate swift demo

---
+++ Example-Swift/Module/Message/Controller/MessageViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MessageViewController.swift
+//  SwiftDemo
+//
+//  Created by Anthony on 2017/12/29.
+//  Copyright © 2017年 Anthony. All rights reserved.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+//    private weak var myLabel1:UILabel!
+//    private weak var myLabel2:UILabel!
+//    private weak var myLabel3:UILabel!
+//    private weak var myLabel4:UILabel!
+//    private weak var myLabel5:UILabel!
+//    private weak var myLabel6:UILabel!
+//    
+//    var buttonColor: UIColor {
+//        return UIColor(named: "buttonColor")!
+//    }
+//    
+//    var labelColor: UIColor {
+//        return UIColor(named: "labelColor")!
+//    }
+//    
+//    var textFieldColor: UIColor {
+//        return UIColor(named: "textFieldColor")!
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.gray
+    }
+}
