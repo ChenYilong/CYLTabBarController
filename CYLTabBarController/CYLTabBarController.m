1aa5882788890c70e5473783caf7bba73d62d028
ixueadev <ixueadev@163.com>
2021-12-01 13:10:22 +0800

    fix AppDelegate without window property crash.

--- CYLTabBarController/CYLTabBarController.m
+++ CYLTabBarController/CYLTabBarController.m
@@ -749,7 +749,10 @@ static void * const CYLTabImageViewDefaultOffsetContext = (void*)&CYLTabImageVie
 
 static inline UIWindow *getMainWindow(){
    UIWindow *window = nil;
-    window = UIApplication.sharedApplication.delegate.window;
+    if ([UIApplication.sharedApplication.delegate respondsToSelector:@selector(window)]) {
+        //兼容新版项目结构，也就是AppDelegate没有window的情况
+        window = UIApplication.sharedApplication.delegate.window;
+    }
     if (!window) {
         if (@available(iOS 13.0, *))
            {
